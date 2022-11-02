711f65fd4ed99836ad0da241aebf0c60a199dae9
AyesC <hoganliam606@gmail.com>
2022-11-01 21:39:36 -0400

    Add project files.

---
+++ Onyix/Client.cs
@@ -0,0 +1,54 @@
+﻿using Discord;
+using Discord.WebSocket;
+using System.Threading.Tasks;
+
+namespace Onyix
+{
+	public class Client
+	{
+		private readonly DiscordSocketClient client;
+
+		public Client()
+		{
+			client = new();
+			client.Log += Log;
+		}
+
+		public async Task Start()
+		{
+			//await client.LoginAsync(TokenType.Bot, "");
+			//await client.StartAsync();
+			await Task.Delay(-1);
+		}
+
+		public async Task Log(LogMessage message)
+		{
+			switch (message.Severity)
+			{
+				case LogSeverity.Verbose:
+					Program.Logs.Debug(exception: message.Exception, "{0}: {1}", message.Source, message.Message);
+					break;
+
+				case LogSeverity.Debug:
+					Program.Logs.Debug(exception: message.Exception, "{0}: {1}", message.Source, message.Message);
+					break;
+
+				case LogSeverity.Info:
+					Program.Logs.Info(exception: message.Exception, "{0}: {1}", message.Source, message.Message);
+					break;
+
+				case LogSeverity.Warning:
+					Program.Logs.Warn(exception: message.Exception, "{0}: {1}", message.Source, message.Message);
+					break;
+
+				case LogSeverity.Error:
+					Program.Logs.Error(exception: message.Exception, "{0}: {1}", message.Source, message.Message);
+					break;
+
+				case LogSeverity.Critical:
+					Program.Logs.Fatal(exception: message.Exception, "{0}: {1}", message.Source, message.Message);
+					break;
+			}
+		}
+	}
+}
---
+++ Onyix/Program.cs
@@ -0,0 +1,36 @@
+﻿using NLog;
+using NLog.Config;
+using System.Reflection;
+using System.Threading.Tasks;
+
+namespace Onyix
+{
+	public class Program
+	{
+		private static readonly Logger logs = LogManager.GetCurrentClassLogger();
+		private static readonly Configuration config = Configuration.Load(HardVars.ConfigPath);
+
+		public static Task Main(string[] args)
+		{
+			LogManager.Configuration = new XmlLoggingConfiguration(HardVars.NLogConfigPath);
+			Logs.Info("Onyix version " + Version);
+
+			return new Client().Start();
+		}
+
+		public static Logger Logs
+		{
+			get => logs;
+		}
+
+		public static Configuration Config
+		{
+			get => config;
+		}
+
+		public static string Version
+		{
+			get => Assembly.GetEntryAssembly().GetName().Version.ToString();
+		}
+	}
+}
