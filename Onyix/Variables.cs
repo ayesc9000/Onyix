c1cc06cdf6202e2de62e8f3d4659b096c0fab329
Liam Hogan <hoganliam606@gmail.com>
2023-01-15 15:06:47 -0500

    Add environment variables class

---
+++ Onyix/Variables.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Onyix
+{
+	public static class Variables
+	{
+		public static string? Token => Environment.GetEnvironmentVariable("TOKEN");
+		public static string? Guild => Environment.GetEnvironmentVariable("GUILD");
+		public static string? Database => Environment.GetEnvironmentVariable("DATABASE");
+	}
+}
