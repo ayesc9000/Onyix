5a34c491f24038aae49b49a582541505fca2b28d
AyesC <hoganliam606@gmail.com>
2023-04-02 21:05:48 -0400

    Cleanup roll command

--- Onyix/Commands/Roll.cs
+++ Onyix/Commands/Roll.cs
@@ -20,66 +20,76 @@ using DSharpPlus.SlashCommands;
 using System;
 using System.Threading.Tasks;
 
-namespace Onyix.Commands
+namespace Onyix.Commands;
+
+/// <summary>
+/// A command module for rolling dice
+/// </summary>
+public class Roll : ApplicationCommandModule
 {
-	public class Roll : ApplicationCommandModule
+	/// <summary>
+	/// Reply to an interaction to roll a die
+	/// </summary>
+	/// <param name="ctx">The context for this interaction</param>
+	/// <param name="amount">The amount of dice to roll</param>
+	/// <param name="sides">The amount of sides per die</param>
+	/// <param name="starting">Add or subtract a specific amount from the result</param>
+	/// <returns>An asynchronous task representing this reply</returns>
+	[SlashCommand("roll", "Rolls a dice", true)]
+	public static async Task RollDie(InteractionContext ctx,
+		[Option("amount", "The amount of dice to roll")] long amount,
+		[Option("sides", "The amount of sides per die")] long sides,
+		[Option("starting", "Add or subtract a specific amount from the result")] long starting)
 	{
-		[SlashCommand("roll", "Rolls a dice", true)]
-		public static async Task Execute(InteractionContext ctx,
-			[Option("amount", "The amount of dice to roll")] long amount,
-			[Option("sides", "The amount of sides per die")] long sides,
-			[Option("starting", "Add or subtract a specific amount from the result")] long starting)
+		// Check if values are within acceptable range
+		// These are here to prevent someone from attempting to create a
+		// result that overflows the maximum amount allowed in a 32-bit int
+		if (amount is < 1 or > 128)
 		{
-			// Check if values are within acceptable range
-			// These are here to prevent someone from attempting to create a
-			// result that overflows the maximum amount allowed in a 32-bit int
-			if (amount < 1 || amount > 128)
-			{
-				await ctx.CreateResponseAsync(new DiscordEmbedBuilder()
-					.WithTitle("Error")
-					.WithDescription("You may only roll 1 to 128 dice at a time.")
-					.WithColor(Colors.Red), true);
-
-				return;
-			}
-
-			if (sides < 2 || sides > 128)
-			{
-				await ctx.CreateResponseAsync(new DiscordEmbedBuilder()
-					.WithTitle("Error")
-					.WithDescription("Dice may only have 2 to 128 sides.")
-					.WithColor(Colors.Red), true);
-
-				return;
-			}
-
-			if (starting < -10000 || starting > 10000)
-			{
-				await ctx.CreateResponseAsync(new DiscordEmbedBuilder()
-					.WithTitle("Error")
-					.WithDescription("The starting value must be within -10,000 to 10,000.")
-					.WithColor(Colors.Red), true);
-
-				return;
-			}
-
-			// Calculate result
-			int result = (int)starting;
-			Random rand = new();
-
-			for (long i = 0; i < amount; i++)
-			{
-				result += rand.Next(1, (int)sides);
-			}
-
-			// Reply with embed
 			await ctx.CreateResponseAsync(new DiscordEmbedBuilder()
-				.WithTitle("Dice roll")
-				.WithColor(Colors.Grey)
-				.AddField("Dice", amount.ToString(), true)
-				.AddField("Sides", sides.ToString(), true)
-				.AddField("Starting value", starting.ToString(), true)
-				.AddField("Result", result.ToString(), false));
+				.WithTitle("Error")
+				.WithDescription("You may only roll 1 to 128 dice at a time.")
+				.WithColor(Colors.Red), true);
+
+			return;
 		}
+
+		if (sides is < 2 or > 128)
+		{
+			await ctx.CreateResponseAsync(new DiscordEmbedBuilder()
+				.WithTitle("Error")
+				.WithDescription("Dice may only have 2 to 128 sides.")
+				.WithColor(Colors.Red), true);
+
+			return;
+		}
+
+		if (starting is < (-10000) or > 10000)
+		{
+			await ctx.CreateResponseAsync(new DiscordEmbedBuilder()
+				.WithTitle("Error")
+				.WithDescription("The starting value must be within -10,000 to 10,000.")
+				.WithColor(Colors.Red), true);
+
+			return;
+		}
+
+		// Calculate result
+		int result = (int)starting;
+		Random rand = new();
+
+		for (long i = 0; i < amount; i++)
+		{
+			result += rand.Next(1, (int)sides);
+		}
+
+		// Reply with embed
+		await ctx.CreateResponseAsync(new DiscordEmbedBuilder()
+			.WithTitle("Dice roll")
+			.WithColor(Colors.Grey)
+			.AddField("Dice", amount.ToString(), true)
+			.AddField("Sides", sides.ToString(), true)
+			.AddField("Starting value", starting.ToString(), true)
+			.AddField("Result", result.ToString(), false));
 	}
 }
